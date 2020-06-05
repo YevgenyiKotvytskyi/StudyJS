53e9cacd2d2a267edb2f57500ad1a5076761bc25
Yevgenyi Kotvytskyi <yegeniy.kotvytskyi@gmail.com>
2020-06-05 16:55:54 +0300

    First ommit lesson 1

---
+++ lesson1.js
@@ -0,0 +1,10 @@
+let money = 100, 
+   income = 2000, 
+   addExpenses = 50, 
+   deposit = 9999, 
+   mission = 'Study hard with good results!', 
+   period = Date();
+
+alert(mission);
+
+console.log(period);
