cf0a6b1443770c196c790a235441b788bb1e782d
Yevgenyi Kotvytskyi <yegeniy.kotvytskyi@gmail.com>
2020-06-08 13:22:28 +0300

    Pretty format for lesson1.js

--- lesson1.js
+++ lesson1.js
@@ -4,28 +4,28 @@ alert, confirm, console, prompt
 
 "use strict"
 
-let money = 1500, 
-   income = 900, 
-   addExpenses = '250, 77, 128.5', 
-   deposit = true, 
-   mission = 10000, 
-   period = 2;
+let money = 1500,
+    income = 900,
+    addExpenses = '250, 77, 128.5',
+    deposit = true,
+    mission = 10000,
+    period = 2;
 
 //#region Lesson 02
 
-console.log( typeof money );
-console.log( typeof income );
-console.log( typeof deposit );
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 
-console.log( addExpenses.length );
-console.log( `Период равен ${period} месяцев` );
-console.log('Цель заработать '+ mission 
+console.log(addExpenses.length);
+console.log(`Период равен ${period} месяцев`);
+console.log('Цель заработать ' + mission
     + ' рублей/долларов/гривен/юани');
 
 let arrAddExpeses = addExpenses.toLowerCase().split(', ');
-console.log( arrAddExpeses );
+console.log(arrAddExpeses);
 
-let budgetDay  = income / 30;    
+let budgetDay = income / 30;
 console.log('budgetDay: ', budgetDay);
 
 //#endregion Lesson 02
@@ -44,10 +44,10 @@ money = parseFloat(prompt("Ваш месячный доход?"));
 // #2
 addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую?");
 // 3
-let depositAnswer  = prompt("Есть ли у вас депозит в банке? (Да/Нет)");
+let depositAnswer = prompt("Есть ли у вас депозит в банке? (Да/Нет)");
 console.log('depositAnswer: ', depositAnswer.trim().toLowerCase());
 
-if (depositAnswer.trim().toLowerCase() === "да" ) {
+if (depositAnswer.trim().toLowerCase() === "да") {
     deposit = true;
 } else if (depositAnswer.trim().toLowerCase() === "нет") {
     deposit = false;
@@ -57,7 +57,7 @@ if (depositAnswer.trim().toLowerCase() === "да" ) {
 console.log('deposit: ', deposit);
 
 // #5
-let 
+let
     expenses1 = prompt("Введите обязательную статью расходов?"),
     amount1 = parseFloat(prompt("Во сколько это обойдется?")),
     expenses2 = prompt("Введите обязательную статью расходов?"),
@@ -78,20 +78,20 @@ let budgetMonth = money - obligatoryExpends;
 console.log('budgetMonth: ', budgetMonth);
 
 //#7
-let monthMission = Math.ceil( mission / budgetMonth  );
+let monthMission = Math.ceil(mission / budgetMonth);
 console.log('monthMission: ', monthMission);
 
 //#8
-budgetDay  = budgetMonth / 30; 
-console.log('budgetDay: ', Math.floor( budgetDay) );
+budgetDay = budgetMonth / 30;
+console.log('budgetDay: ', Math.floor(budgetDay));
 
 //#9
 
-if ( budgetDay >= 1200) {
+if (budgetDay >= 1200) {
     console.log("У Вас высокий уровень дохода");
-} else if ( budgetDay >= 600 ) {
+} else if (budgetDay >= 600) {
     console.log("У вас средний уровень дохода");
-} else if ( budgetDay >= 0 ) {
+} else if (budgetDay >= 0) {
     console.log("К сожалению у вас уровень дохода ниже среднего");
 } else {
     console.log("Что то пошло не так");
