76dfcec6b2b122f7d8266e2035a07a65afd6b450
Yevgenyi Kotvytskyi <yegeniy.kotvytskyi@gmail.com>
2020-07-08 17:08:41 +0300

    Start lesson_21_1

--- script/script.js
+++ script/script.js
@@ -392,6 +392,55 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     imageHover();
-    ////#endregion images
+    //#endregion images
+
+    //#region calc
+    const calc = (price = 100)  => {
+
+        const calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcCount = document.querySelector('.calc-count'),
+            calcDay = document.querySelector('.calc-day'),
+            totalValue = document.getElementById('total');
+
+        const handlerCalc = e => {
+            const target = e.target;
+            let total = 0,
+                countValue = 1,
+                dayValue = 1;
+            if (target.matches('select') || target.matches('input')) {
+                const typeValue = calcType.options[calcType.selectedIndex].value,
+                    squareValue = +calcSquare.value;
+                if (calcCount.value) {
+                    countValue += (calcCount.value - 1) / 10;
+                }
+
+                if (calcDay.value) {
+                    if (calcDay.value < 5) {
+                        dayValue *= 2;
+                    } else if (calcDay.value < 10) {
+                        dayValue *= 1.5;
+                    }
+
+                }
+                if (typeValue && squareValue)  {
+                    total = price * typeValue * squareValue * countValue * dayValue;
+                }
+            }
+
+            totalValue.textContent = total;
+
+        };
+
+        calcBlock.addEventListener('change', handlerCalc);
+
+    };
+
+    calc(100);
+
+
+    //#endregion calc
+
 
 });
