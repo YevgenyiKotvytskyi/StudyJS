442a780b9811874c4d6bbd4591ea616121a135a3
Yevgenyi Kotvytskyi <yegeniy.kotvytskyi@gmail.com>
2020-07-09 22:33:35 +0300

    Start lesson_26_2

--- script/script.js
+++ script/script.js
@@ -480,12 +480,54 @@ window.addEventListener('DOMContentLoaded', () => {
         `font-size: 2rem;
         color: white;`;
 
+        const createPreloader = () => {
+            document.head.insertAdjacentHTML('beforeEnd', '<link rel="stylesheet" href="./css/preloader.css">');
+            const element = document.createElement('section');
+            // element.style.height = form.offsetHeight + 'px';
+            element.classList.add('preloader');
+            element.innerHTML = `
+                <div class='sk-circle-bounce'>
+                <div class='sk-child sk-circle-1'></div>
+                <div class='sk-child sk-circle-2'></div>
+                <div class='sk-child sk-circle-3'></div>
+                <div class='sk-child sk-circle-4'></div>
+                <div class='sk-child sk-circle-5'></div>
+                <div class='sk-child sk-circle-6'></div>
+                <div class='sk-child sk-circle-7'></div>
+                <div class='sk-child sk-circle-8'></div>
+                <div class='sk-child sk-circle-9'></div>
+                <div class='sk-child sk-circle-10'></div>
+                <div class='sk-child sk-circle-11'></div>
+                <div class='sk-child sk-circle-12'></div>
+                </div>
+            `;
+            element.style.display = 'none';
+            return element;
+        };
+
+        const preloaderElement = createPreloader();
+
+        const showPreloader = (show = true) => {
+            if (show) {
+                form.appendChild(preloaderElement);
+                preloaderElement.style.top =  `-${form.offsetHeight / 2 + 30}px`;
+                preloaderElement.style.display = 'block';
+                preloaderElement.style.marginBottom = '-' + preloaderElement.offsetHeight + 'px';
+            } else {
+                preloaderElement.style.display = 'none';
+            }
+            [...form.elements].forEach(elem => {
+                elem.disabled = show;
+            });
+        };
+
+
         form.addEventListener('submit', e => {
 
             e.preventDefault();
             // eslint-disable-next-line no-use-before-define
             if (!isValid()) return;
-            form.appendChild(statusMessage);
+            showPreloader();
 
             const formData = new FormData(form);
             const body = {};
@@ -494,17 +536,20 @@ window.addEventListener('DOMContentLoaded', () => {
                 body[key] = val;
             });
 
-            statusMessage.textContent = loadMessage;
-
             // eslint-disable-next-line no-use-before-define
             postData(body,
                 () => {
+                    showPreloader(false);
                     statusMessage.textContent = successMessage;
+                    form.appendChild(statusMessage);
                     [...form.elements].forEach(elem => {
                         if (elem.value) elem.value = '';
                     });
+
                 },
                 error => {
+                    showPreloader(false);
+                    form.appendChild(statusMessage);
                     statusMessage.textContent = errorMessage;
                     console.error(error);
                 }
